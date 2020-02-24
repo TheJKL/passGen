6e6db2eade4c1b4c0489e3c2e4e78429461dfc13
TheJKL <vanjackal@protonmail.com>
2020-02-23 20:24:41 -0600

    changed function to prepare for command line input

--- passGen.py
+++ passGen.py
@@ -4,20 +4,21 @@ import random
 with open("passWordlist.txt") as file:
 	wordlist = file.readlines()
 
-symbols = ["(","#",")","%","^"]
 
 
 
-def getPass(n, min = 0, max = 100,numMax = 99):
+
+def getPass(n, min = 0, max = 100,numMax = 99,seperator = "_",symbols = ["(","#",")","%","^"]):
 	if min == 0: min = n*4
 	passw = ""
 	for _ in range(n-1):
-		passw += f"_{random.choice(wordlist).strip()}"
+		passw += f"{seperator}{random.choice(wordlist).strip()}"
 	passw += f"{random.choice(wordlist).strip().capitalize()}{random.choice(symbols)}{random.randint(1,numMax)}"
 	l = len(passw)
 	if l >= min and l <= max:
 		return passw
 	else:
 		return getPass(n,min,max)
-	
+
+
 print(getPass(4,max = 30))
